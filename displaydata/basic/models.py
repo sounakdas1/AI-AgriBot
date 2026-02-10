6c0a1373c1d5edd14f5132c7f7284b9b68bb82c1
Sounak Das <dassounak537@gmail.com>
2026-02-10 22:57:15 +0530

    view done

--- displaydata/basic/models.py
+++ displaydata/basic/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class MyData(models.Model):
     temp = models.FloatField()
     time = models.DateTimeField(auto_now_add=True)
+
+
+
--- displaydata/basic/views.py
+++ displaydata/basic/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+import json
+
+def showdata(request):
+    if request.method=='POST':
+        data = json.loads(request.body)
+
+        temp = data.get('temperature')
+
+        print("temp", temp)
+    return render(request,'show.html',{"temp":temp})
