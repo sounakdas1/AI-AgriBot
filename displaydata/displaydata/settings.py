c51468edc08ea4f875710830b8942b54a3fcc7eb
Sounak Das <dassounak537@gmail.com>
2026-02-11 19:24:51 +0530

    yes

--- displaydata/displaydata/settings.py
+++ displaydata/displaydata/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+p&3jj%^36&*6g8gai1gsz5&-orth_+%&jv@^6=w(oj*q%b(no'
+
 from dotenv import load_dotenv
 import os
 
